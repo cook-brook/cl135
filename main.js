73208a8f0afe821b9fc4a6ab4daaaa9b7bd4d82e
cook-brook <72130874+cook-brook@users.noreply.github.com>
2021-09-17 16:30:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 video="";
 status="";
+objects=[];
 function preload(){
     video=createVideo('video.mp4');
     video.hide();
@@ -11,6 +12,27 @@ function setup(){
 }
 function draw(){
     image(video,0,0,480,380);
+
+    if (status!=""){
+        objectDetector.detect(video,gotResults);
+        for(i=0; i<objects.length;i++){
+            document.getElementById("status").innerHTML="Status: Video detected";
+            document.getElementById("number_of_objects").innerHTML="Number of object detected are "+objects.length;
+
+            fill("#FFC0CB");
+            percentage=floor(objects[i].confidence*100);
+            text(objects[i].label+" "+ percentage+"%",objects[i].x,objects[i].y);
+            noFill();
+            rect(objects[i].width,objects[i].height,objects[i].x,objects[i].y);
+        }
+    }
+}
+function gotResults(error,results){
+    if (error){
+        console.log(error);
+    }
+    console.log(results);
+    objects=results;
 }
 function start(){
     objectDetector=ml5.objectDetector('cocossd',modelLoaded);
